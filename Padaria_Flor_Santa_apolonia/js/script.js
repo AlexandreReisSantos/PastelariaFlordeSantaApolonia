1aa37536de91fbea976457a144e5eee04263f89c
Alexandre Felipe dos Reis Santos <ale.r.santos11@gmail.com>
2022-12-25 21:02:16 +0000

    Padaria.v7

--- Padaria_Flor_Santa_apolonia/js/script.js
+++ Padaria_Flor_Santa_apolonia/js/script.js
@@ -35,3 +35,28 @@ function openModal() {
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
   }
+
+  function carregar() {
+    var url = 'https://news.google.com/rss?hl=pt‐PT&gl=PT&ceid=PT:pt‐150';
+    $.ajax({
+        url:
+"https://api.rss2json.com/v1/api.json?rss_url=" + url,
+        type: 'GET',
+        success: function (data) {
+            objeto_json = eval(data);
+            // ler o conteúdo
+            var frase = "";
+            for (i = 0; i < objeto_json.items.length; i++)
+{
+                frase = frase + "Título: <b>" +
+objeto_json.items[i].title + "</b><br/>";
+                frase = frase +
+objeto_json.items[i].description + "<br/>";
+            }
+            $("#caixa02").html(frase);
+          },
+          error: function (xhr, status) {
+              alert('Ocorreu um erro.');
+          }
+      });
+  }
